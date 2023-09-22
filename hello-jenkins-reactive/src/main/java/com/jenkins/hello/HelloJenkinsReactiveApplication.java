6859ec41e7b32ae93bfcdd37ad88c9462582ded9
venktsGitHub <93146094+venktsGitHub@users.noreply.github.com>
2023-09-22 18:02:04 +0530

    Add files via upload

---
+++ hello-jenkins-reactive/src/main/java/com/jenkins/hello/HelloJenkinsReactiveApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.hello;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloJenkinsReactiveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloJenkinsReactiveApplication.class, args);
+	}
+
+}
---
+++ hello-jenkins/src/main/java/com/jenkins/hello/HelloJenkinsApplication.java
@@ -0,0 +1,17 @@
+package com.jenkins.hello;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class HelloJenkinsApplication {
+
+//	public Logger log = LoggerFactory.
+	public static void main(String[] args) {
+		
+		SpringApplication.run(HelloJenkinsApplication.class, args);
+	}
+
+}
